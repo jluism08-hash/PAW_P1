f7d35c8e42e0d8320b17baf2d2cf28032248d06a
joseluis.miranda@uned.cr <joseluis.miranda@uned.cr>
2025-10-23 13:44:02 -0600

    Agregar archivos de proyecto.

---
+++ PAW_P1/Models/Docente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAW_P1.Models
+{
+    public class Docente
+    {
+        public int IdDocente { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+    }
+}
---
+++ PAW_P1/Models/Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAW_P1.Models
+{
+    public class Estudiante
+    {
+        public int IdEstudiante { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Identificacion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Provincia { get; set; }
+        public string Canton { get; set; }
+        public string Distrito { get; set; }
+        public string Correo { get; set; }
+    }
+}
